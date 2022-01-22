d5febee8ad764056612bc28b860165d79a9f0f6c
bianchithomas <98235730+bianchithomas@users.noreply.github.com>
2022-01-22 16:20:01 -0500

    Add files via upload

---
+++ FirstBank/src/FirstBank/FirstBank.java
@@ -0,0 +1,12 @@
+package FirstBank;
+import java.util.Scanner;
+
+public class FirstBank {
+	
+	public static void main (String[] args) {
+		
+		Account thomas = new Account("Thomas", "001");
+		thomas.start();
+	}
+
+}
