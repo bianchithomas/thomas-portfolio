e71b65315b6bd8d153b445fffeed10bc5af057b3
bianchithomas <thomasisprogramming@gmail.com>
2022-01-22 16:40:03 -0500

    new Account object, James

--- FirstBank/src/FirstBank/FirstBank.java
+++ FirstBank/src/FirstBank/FirstBank.java
@@ -5,8 +5,11 @@ public class FirstBank {
 	
 	public static void main (String[] args) {
 		
-		Account thomas = new Account("Thomas", "001");
-		thomas.start();
+//		Account thomas = new Account("Thomas", "001");
+//		thomas.start();
+		
+		Account james = new Account ("James", "002");
+		james.start();
 	}
 
 }
